ce01526639c3353a45493b504efc40cb28157e4f
jiseok <jiseok762@naver.com>
2023-08-31 15:52:14 +0900

    SentiWord_info.json 데이터 추가 및 구현 함수(data_preprocessing)에 추가

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,6 +1,9 @@
 import pandas as pd
+import re
+import json
 import numpy as np
 from konlpy.tag import Twitter
+from wordcloud import WordCloud
 
 class dataPreprocessingCls:
     def __init__(self):
@@ -14,6 +17,23 @@ class dataPreprocessingCls:
         self.stock_df = pd.read_csv('./stockdata/stock.csv')
         self.daum_df = pd.read_csv('./daum/output.csv', dtype=object)
 
+    def data_list(self, wordname):
+        with open('data/SentiWord_info.json', encoding='utf-8-sig', mode='r') as f:
+            data = json.load(f)
+
+        result = ['None', 'None']
+        for i in range(0, len(data)):
+            if data[i]['word'] == wordname:
+                result.pop()
+                result.pop()
+                result.append(data[i]['word_root'])
+                result.append(data[i]['polarity'])
+
+        r_word = result[0]
+        s_word = result[1]
+
+        print('어근 : ' + r_word)
+        print('극성 : ' + s_word)
 
     def stockPreprocessing(self): # 주식 데이터 전처리
         pass
@@ -21,22 +41,31 @@ class dataPreprocessingCls:
     def discussionPreprocessing(self): # 주식 + 종토방 데이터 전처리
         df = self.daum_df.copy()
         df = df[['Title','Contents']]
-        twt = Twitter()
 
+        '''
         # 형태소 분석
+        twt = Twitter()
         sy = []
         for i in df['Contents']:
             tagging = twt.pos(i)
-            print(tagging)
             for i, j in tagging:
                 # '동사', '명사'만 추출
                 if j == 'Noun' or j == 'Verb':
                     sy.append(i)
+        print(sy)
+        '''
 
-
+        # 문자열 분석
+        for content in df['Contents']:
+            # '%'를 제외한 모든 특수문자를 제거
+            result = re.sub(r'[^\w\s%]', '', content)
+            # 영어 알파벳을 제거
+            result = re.sub(r'[a-zA-Z]', '', result)
+            print(result)
 
         #df = pd.merge(self.stock_df, self.discuss_df, how='inner', on=['Code', 'Date'])
         #print(df)
 
 st = dataPreprocessingCls()
-st.discussionPreprocessing()
+#st.discussionPreprocessing()
+st.data_list('가년스럽다')
